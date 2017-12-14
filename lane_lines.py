50529edf8ea2bb385e1b752a79631b3854fdf3a1
Samkit Jain <samkitjain_2k14@dtu.ac.in>
2017-12-14 13:55:46 +0100

    Update lane_lines.py

--- lane_lines.py
+++ lane_lines.py
@@ -394,97 +394,14 @@ def pipeline(warped,image,left_fit,right_fit,count):
         right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
         middle = (left_fitx[-1] + right_fitx[-1])//2
         veh_pos = image.shape[1]//2
-        dx = (veh_pos - middle)*xm_per_pix # Positive if on right, Negative on left
+        difference = (veh_pos - middle)*xm_per_pix # Positive if on right, Negative on left
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(result,'Left radius of curvature  = %.2f m'%(left_curverad),(50,50), font, 1,(255,255,255),2,cv2.LINE_AA)
         cv2.putText(result,'Right radius of curvature = %.2f m'%(right_curverad),(50,80), font, 1,(255,255,255),2,cv2.LINE_AA)
-        cv2.putText(result,'Vehicle position : %.2f m %s of center'%(abs(dx), 'left' if dx < 0 else 'right'),(50,110),
+        cv2.putText(result,'Vehicle at : %.2f m %s of center'%(abs(difference), 'left_side' if difference < 0 else 'right_side'),(50,110),
                         font, 1,(255,255,255),2,cv2.LINE_AA)
-        count=count+1
         return result
-    else:
-        nonzero = binary_warped.nonzero()
-        nonzeroy = np.array(nonzero[0])
-        nonzerox = np.array(nonzero[1])
-        margin = 100
-        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + 
-        left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + 
-        left_fit[1]*nonzeroy + left_fit[2] + margin))) 
-
-        right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + 
-        right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + 
-        right_fit[1]*nonzeroy + right_fit[2] + margin)))  
-
-        # Again, extract left and right line pixel positions
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds] 
-        rightx = nonzerox[right_lane_inds]
-        righty = nonzeroy[right_lane_inds]
-        # Fit a second order polynomial to each
-        left_fit = np.polyfit(lefty, leftx, 2)
-        right_fit = np.polyfit(righty, rightx, 2)
-        # Generate x and y values for plotting
-        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-        warp_zero = np.zeros_like(warped).astype(np.uint8)
-        color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-        # Minv = cv2.getPerspectiveTransform(dst, src)
-        # Recast the x and y points into usable format for cv2.fillPoly()
-        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
-        pts = np.hstack((pts_left, pts_right))
-
-        # Draw the lane onto the warped blank image
-        cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
-
-        # Warp the blank back to original image space using inverse perspective matrix (Minv)
-        newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0])) 
-        # Combine the result with the original image
-        result = cv2.addWeighted(image, 1, newwarp, 0.3, 0)
-        
-        ploty = np.linspace(0, 719, num=720)# to cover same y-range as image
-        quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
-        # For each y position generate random x position within +/-50 pix
-        # of the line base position in each case (x=200 for left, and x=900 for right)
-        leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
-                                      for y in ploty])
-        rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
-                                        for y in ploty])
-
-        leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
-        rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
-
-
-        # Fit a second order polynomial to pixel positions in each fake lane line
-        left_fit = np.polyfit(ploty, leftx, 2)
-        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-        right_fit = np.polyfit(ploty, rightx, 2)
-        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-        y_eval = np.max(ploty)
-        left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
-        right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
-        
-        ym_per_pix = 30/720 # meters per pixel in y dimension
-        xm_per_pix = 3.7/700 # meters per pixel in x dimension
-
-        # Fit new polynomials to x,y in world space
-        left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
-        right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
-        # Calculate the new radii of curvature
-        left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
-        right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
-        middle = (left_fitx[-1] + right_fitx[-1])//2
-        veh_pos = image.shape[1]//2
-        dx = (veh_pos - middle)*xm_per_pix # Positive if on right, Negative on left
-        
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(result,'Left radius of curvature  = %.2f m'%(left_curverad),(50,50), font, 1,(255,255,255),2,cv2.LINE_AA)
-        cv2.putText(result,'Right radius of curvature = %.2f m'%(right_curverad),(50,80), font, 1,(255,255,255),2,cv2.LINE_AA)
-        cv2.putText(result,'Vehicle position : %.2f m %s of center'%(abs(dx), 'left' if dx < 0 else 'right'),(50,110),
-                            font, 1,(255,255,255),2,cv2.LINE_AA)
-        return result
-
+    
 
 # In[23]:
 
